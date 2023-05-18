bbcb4a73ee4a697f5a1109b526e2a11428716cdc
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-18 16:45:28 +0300

    Restored work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
+import { Component } from 'react'
+import { Section } from './Section/Section';
+import { PageContainer } from './PageContainer/PageContainer';
+import { GlobalStyle } from './GlobalStyle';
+import { Statistic } from './Statistic/Statistic';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = e => {
+    this.setState({ [e]: this.state[e] + 1});
+  }
+
+  countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+
+  countPositiveFeedbackPercentage = ({ good }) =>
+  Math.round((good * 100) / this.countTotalFeedback(this.state));
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <PageContainer>
+        <Section title="Please leave feedback">
+          <FeedbackOptions  
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={this.leaveFeedback}>
+          </FeedbackOptions>      
+          </Section>
+          <Section title="Statistic">
+            <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback(this.state)}
+            positivePercentage={this.countPositiveFeedbackPercentage(
+              this.state
+            )}>
+            </Statistic>
+          </Section>
+        <GlobalStyle />
+    </PageContainer>    
+  );}
+}
+ 
